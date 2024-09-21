ab4bf8f794ab1e81a137d4d70d4debe411fd7c94
siriyakorn achasewin <88705838+ggiftsyk@users.noreply.github.com>
2024-09-21 18:31:49 +0700

    update

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import DetailPage from './DetailPage';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <DetailPage />
+    <PayingPage />
   </React.StrictMode>
 );
 
--- src/pages/receiptPage/ReceiptPage.js
+++ src/pages/receiptPage/ReceiptPage.js
@@ -1,4 +1,4 @@
-import { ChevronDownIcon } from "@heroicons/react/24/outline";
+import { ArrowDownTrayIcon, ChevronDownIcon } from "@heroicons/react/24/outline";
 import React, { useState } from "react";
 import receipt from "../../assets/Receipt.png"
 
@@ -57,9 +57,54 @@ function ReceiptPage() {
                     </div>
                 </div>
 
-                <div className=" pl-9 pr-9 w-full flex flex-col justify-center items-center">
-<h1 className=" w-full flex justify-center">ใบเสร็จการชำระเงิน</h1>
+                <div className=" pl-9 pr-9 w-full flex flex-col justify-center items-center space-y-4">
+                    <h1 className=" w-full flex justify-center">ใบเสร็จการชำระเงิน</h1>
+
+                    <div className=" border-[0.5px] border-gray-300 w-full h-a flex flex-col p-3 space-y-3">
+                        <h2 className=" text-sm w-full flex justify-center">บจก. KMITL สแควร์</h2>
+                        <p className=" text-xs w-ful space-y-1 ">
+                            <p>TAX ID: 0105539025678</p>
+                            <p>Date: 17/08/2024</p>
+                            <p>Rcpt: RM9999240817000658</p>
+                        </p>
+                        <div className=" w-full border-[0.5px] border-gray-300"></div>
+                        <p className=" text-xs w-ful space-y-1 ">
+                            <p>เลขทะเบียน: 2กฐ452 (C)</p>
+                            <p>เวลาเข้า: 17/08/2024 14:32</p>
+                            <p>เวลาออก: 17/08/2024 18:33</p>
+                            <div className=" flex justify-between">
+                                <p>ค่าบริการ</p>
+                                <p>90.00 บาท</p>
+                            </div>
+                            <div className=" flex justify-between">
+                                <p>ชำระเงิน</p>
+                                <p>90.00 บาท</p>
+                            </div>
+                            <div className=" flex justify-between">
+                                <p>วิธีการชำระเงิน</p>
+                                <p>Thai QR PromptPay</p>
+                            </div>
+                        </p>
+                        <div className=" w-full border-[0.5px] border-gray-300"></div>
+                        <div className=" flex flex-col space-y-1">
+                            <h2 className=" text-md w-full flex justify-center">กรุณานำรถออกก่อนเวลา 19:32</h2>
+                            <h3 className=" text-xs w-full flex justify-center text-gray-500">หากเกินเวลาจะคิดค่าบริการเพิ่ม</h3>
+                        </div>
+                    </div>
+
+                    <div className=" flex flex-col justify-center items-center w-full h-auto  ">
+                            <button className=" rounded-2xl border-[0.5px] p-2 border-gray-300 w-8 h-8 flex justify-center items-center">
+                                <ArrowDownTrayIcon className=" w-auto h-auto" />
+                            </button>
+                            <p className=" mt-2 text-sm">บันทึก QR</p>
+                        </div>
                 </div>
+            </div>
+
+            <div className=" flex flex-col justify-center items-end space-y-4 w-full pt-6 pb-6 absolute bottom-0 p-4 border-t-[0.5px] border-gray-300 " style={{ boxShadow: '0 -2px 6px rgba(209, 213, 219, 1)' }}>
+                <button className=" w-full flex justify-center items-center bg-[#007AFF] text-white p-3 rounded-[20px] shadow-md font-medium text-md">
+                    เสร็จสิ้น
+                </button>
             </div>
         </div>
     )
